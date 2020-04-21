ba007443612601e7c6c556fc9655a2a7805e69ff
zhouf <zhouf@Franz>
2020-04-21 12:23:37 +0200

    programmazione dinamica

--- programmiJava/src/rompicapoNRegine/Queens.java
+++ programmiJava/src/rompicapoNRegine/Queens.java
@@ -14,11 +14,19 @@ public class Queens {
 		return numeroDiCompletamenti(new Board(n));
 	}	// method numeroDiSoluzioni
 	
-	public static StringSList listaDiSoluzioni(int n) {
-		return listaDiCompletamenti(new Board(n));
+	public static StringSList listaDiSoluzioni2(int n) {
+		return listaDiCompletamenti2(new Board(n));
+	}	// method listaDiSoluzioni2
+	
+	public static BoardSList listaDiSoluzioni(int n) {
+		BoardSList bl = new BoardSList();
+		
+		bl = bl.cons(listaDiCompletamenti(new Board(n)));
+		
+		return bl;
 	}	// method listaDiSoluzioni
 	
-	public static StringSList listaDiCompletamenti(Board b) {
+	public static StringSList listaDiCompletamenti2(Board b) {
 		int n = b.size();
 		int q = b.queensOn();
 		
@@ -30,7 +38,7 @@ public class Queens {
 			
 			for (int j = 1; j <= n; j++) {
 				if (!b.underAttack(i, j)) {
-					sl = sl.append(listaDiCompletamenti( b.addQueen(i, j) ));
+					sl = sl.append(listaDiCompletamenti2( b.addQueen(i, j) ));
 				}
 			}
 			return sl;
@@ -38,7 +46,29 @@ public class Queens {
 		
 	}	// method listaDiCompletamenti
 	
-	public static int numeroDiCompletamenti(Board b) {
+	public static Board listaDiCompletamenti(Board b) {
+		int n = b.size();
+		int q = b.queensOn();
+		
+		if ( q == n ) {
+			return b;
+		} else {
+			int i = q + 1;
+			
+			for (int j = 1; j <= n; j++) {
+				if ( !b.underAttack(i, j) ) {
+					//i++;
+					b = listaDiCompletamenti( b.addQueen(i, j) );
+					System.out.println("numero regine: " + b.queensOn());
+					System.out.println("config: " + b.arrangement());
+				}
+			}
+			
+			return b;
+		}
+	}	// method listaDiCompletamenti
+	
+	public static int numeroDiCompletamenti2(Board b) {
 		int n = b.size();
 		int q = b.queensOn();
 		
@@ -57,6 +87,46 @@ public class Queens {
 		}
 		
 	}	// method listaDiCompletamenti
+	
+	public static int numeroDiCompletamenti(Board b) {
+		int n = b.size();
+		int q = b.queensOn();
+		int count = 0;
+		int i = q;
+		
+		for (int j = 0; j < n; j++) {
+			if ( !b.underAttack(i, j) ) {
+				i = q + 1;
+				count++;
+				b.addQueen(i, j);
+			}
+		}
+		return count;
+		
+	}	// method listaDiCompletamenti
+	
+	/*public static BoardSList listaDiBoard(int n) {
+		Board b = new Board(n);
+		//BoardSList bl = new BoardSList();
+		//bl = bl.cons(listaDiSoluzioni(4).toString());
+		
+		int s = b.size();
+		int q = b.queensOn();
+		
+		if (q == s) {
+			return (BoardSList.NULL_BOARDLIST).cons(b);
+		} else {	// q < n
+			int i = q + 1;
+			StringSList sl = StringSList.NULL_STRINGLIST;
+			
+			for (int j = 1; j <= s; j++) {
+				if (!b.underAttack(i, j)) {
+					sl = sl.append(listaDiCompletamenti( b.addQueen(i, j) ));
+				}
+			}
+			return sl;
+		}
+	}*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
